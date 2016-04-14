e99442584b1b9080df748161dc4ed625e914387f
Brian Douglas <ilikerobot@gmail.com>
2016-04-14 06:47:11 -0700

    WIP: serving up JSON

--- json-api/main.go
+++ json-api/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Record struct {
+	id      string `json:"id"`
+	domain  string `json:"domain"`
+	name    string `json:"name"`
+	address string `json:"address"`
+}
+
+type Records []Record
+
 func main() {
 	port := ":3130"
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/records", RecordIndex)
+	http.HandleFunc("/records/{recordId}", RecordShow)
 
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
+	fmt.Fprintln(w, "Hola Mundo!")
 }
 
 func RecordIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "RecordIndex!")
+	records := Records{
+		Record{id: "1", domain: "web.com", name: "A", address: "192.62.1.1"},
+		Record{id: "2", domain: "web.com", name: "CNAME", address: "192.62.1.1"},
+	}
+
+	json.NewEncoder(w).Encode(records)
+}
+
+func RecordShow(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "RecordShow!")
 }
--- json-api/main_test.go
+++ json-api/main_test.go
@@ -18,8 +18,23 @@ func TestGet200(t *testing.T) {
 	}
 }
 
-func TestGet200ForRecordRoute(t *testing.T) {
+func TestGet200ForRecordsRoute(t *testing.T) {
 	req, err := http.NewRequest("GET", "http://localhost:3130/records", nil)
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Success expected: %d", res.StatusCode)
+	}
+}
+
+func TestGet200ForRecordShowRoute(t *testing.T) {
+	t.Skip("this test is not really valid yet, show route is not aware of param")
+
+	req, err := http.NewRequest("GET", "http://localhost:3130/records/1", nil)
 	res, err := http.DefaultClient.Do(req)
 
 	if err != nil {
