940c3f6566ba14c9314130ec59714e2b9e7598d9
Brian Douglas <ilikerobot@gmail.com>
2016-04-15 12:58:50 -0700

    working api consumption

--- client/modules/components/Records/index.js
+++ client/modules/components/Records/index.js
@@ -1,24 +1,27 @@
 import React, {Component, PropTypes} from 'react'
 import Title from 'react-title-component'
-import http from 'http'
 import {resolve} from 'react-resolver'
+import axios from 'axios'
 
 @resolve('records', () => {
-  // records are returning empty
-  return http.get('http://localhost:3130/records')
+  return axios.get('http://localhost:3130/records')
+  .then(({data}) => data)
+  .catch((err) => console.log(err))
 })
 
 export default class Records extends Component {
   static propTypes = {
-    records: PropTypes.object
+    records: PropTypes.array
   }
 
   render() {
     return (
       <div>
         <Title render={prev => `${prev} | Records!`}/>
-        <p>Records will go here</p>
-        {this.renderRecords}
+        <p>Records are list below</p>
+        <div className='records'>
+          {this.renderRecords()}
+        </div>
       </div>
     )
   }
